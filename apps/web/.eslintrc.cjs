f99b99a1be19affd18ce02e2e40081264cbad305
kevinmgt <kevinmarguet25@gmail.com>
2026-05-04 11:42:23 +0200

    ci: unblock docker build and audit report

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['next/core-web-vitals'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
