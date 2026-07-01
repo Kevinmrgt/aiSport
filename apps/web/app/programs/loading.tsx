3e9e301660e33d294fa07260e56b011af84e03b9
Kevin MARGUET <kevin.marguet@azylis.net>
2026-07-01 15:53:19 +0200

    Improve route loading feedback

---
+++ apps/web/app/programs/loading.tsx
@@ -0,0 +1,5 @@
+import { ListLoadingSkeleton } from '@/components/RouteLoading';
+
+export default function ProgramsLoading() {
+  return <ListLoadingSkeleton label="Chargement des programmes en cours..." />;
+}
---
+++ apps/web/app/settings/loading.tsx
@@ -0,0 +1,5 @@
+import { SettingsLoadingSkeleton } from '@/components/RouteLoading';
+
+export default function SettingsLoading() {
+  return <SettingsLoadingSkeleton />;
+}
